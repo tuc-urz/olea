1dc44a7894c2b4433ad28b24b9a232814a50c174
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2025-08-30 22:11:46 +0200

    Use timetable calender in module tabbar

--- apps/app-olea/navigation/MainTabNavigator.js
+++ apps/app-olea/navigation/MainTabNavigator.js
@@ -1,20 +1,20 @@
-import React                        from 'react';
-import { View }                     from 'react-native';
-import { useTranslation }           from 'react-i18next';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View }                                from 'react-native';
+import { useTranslation }                      from 'react-i18next';
+import { createBottomTabNavigator }            from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
-import ConnectivityWarning          from '@openasist/component-connectivity-warning';
+import ConnectivityWarning                     from '@openasist/component-connectivity-warning';
 
-import DashboardStack               from './stacks/dashboard';
-import FeedsStack                   from './stacks/feeds';
-import TimetableListStack           from './stacks/timetable-list';
-import CanteensStack                from './stacks/canteens';
-import MenuStack                    from './stacks/menu';
+import DashboardStack                          from './stacks/dashboard';
+import FeedsStack                              from './stacks/feeds';
+import TimetableListStack                      from './stacks/timetable-list';
+import TimetableCalendarStack                  from './stacks/timetable-calendar';
+import CanteensStack                           from './stacks/canteens';
+import MenuStack                               from './stacks/menu';
 
 const BottomTabs = createBottomTabNavigator();
 
 
-export default ({colors}) => {
+export default ({ colors }) => {
 
     const { t } = useTranslation();
     const insets = useSafeAreaInsets();
@@ -36,15 +36,16 @@ export default ({colors}) => {
                         tabBarHideOnKeyboard: true,
                         tabBarStyle: [{
                             display: 'flex',
-                            height:  tabBarHeight,
+                            height: tabBarHeight,
                             backgroundColor: colors.tabBar
                         }]
                     }}>
-                        <BottomTabs.Screen name="dashboardTab"  options={{...DashboardStack.options, tabBarAccessibilityLabel: t(DashboardStack.options.tabBarAccessibilityLabel)}} accessible={true}>{DashboardStack.stack}</BottomTabs.Screen>
-                        <BottomTabs.Screen name="feedsTab"      options={{...FeedsStack.options, tabBarAccessibilityLabel: t(FeedsStack.options.tabBarAccessibilityLabel)}}>{FeedsStack.stack}</BottomTabs.Screen>
-                        <BottomTabs.Screen name="TimetableList" options={{...TimetableListStack.options, tabBarAccessibilityLabel: t(TimetableListStack.options.tabBarAccessibilityLabel)}}>{TimetableListStack.stack}</BottomTabs.Screen>
-                        <BottomTabs.Screen name="canteensTab"   options={{...CanteensStack.options, tabBarAccessibilityLabel: t(CanteensStack.options.tabBarAccessibilityLabel)}}>{CanteensStack.stack}</BottomTabs.Screen>
-                        <BottomTabs.Screen name="menuTab"       options={{...MenuStack.options, tabBarAccessibilityLabel: t(MenuStack.options.tabBarAccessibilityLabel)}}>{MenuStack.stack}</BottomTabs.Screen>
+                        <BottomTabs.Screen name='dashboardTab'         options={{ ...DashboardStack.options,         tabBarAccessibilityLabel: t(DashboardStack.options.tabBarAccessibilityLabel) }} accessible={true}>{ DashboardStack.stack         }</BottomTabs.Screen>
+                        <BottomTabs.Screen name='feedsTab'             options={{ ...FeedsStack.options,             tabBarAccessibilityLabel: t(FeedsStack.options.tabBarAccessibilityLabel) }}                      >{ FeedsStack.stack             }</BottomTabs.Screen>
+                    {/* <BottomTabs.Screen name='timetableListTab'     options={{ ...TimetableListStack.options,     tabBarAccessibilityLabel: t(TimetableListStack.options.tabBarAccessibilityLabel) }}              >{ TimetableListStack.stack     }</BottomTabs.Screen> */}
+                        <BottomTabs.Screen name='timetableCalendarTab' options={{ ...TimetableCalendarStack.options, tabBarAccessibilityLabel: t(TimetableCalendarStack.options.tabBarAccessibilityLabel) }}          >{ TimetableCalendarStack.stack }</BottomTabs.Screen>
+                        <BottomTabs.Screen name='canteensTab'          options={{ ...CanteensStack.options,          tabBarAccessibilityLabel: t(CanteensStack.options.tabBarAccessibilityLabel) }}                   >{ CanteensStack.stack          }</BottomTabs.Screen>
+                        <BottomTabs.Screen name='menuTab'              options={{ ...MenuStack.options,              tabBarAccessibilityLabel: t(MenuStack.options.tabBarAccessibilityLabel) }}                       >{ MenuStack.stack              }</BottomTabs.Screen>
                     </BottomTabs.Navigator>
                 </ConnectivityWarning>
             </View>
