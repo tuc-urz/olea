c39d14e246012426a63affac86fd18242ed0685f
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2026-02-27 21:21:07 +0100

    Move export in central olea base index.js

---
+++ packages/index.js
@@ -0,0 +1,44 @@
+
+// Exports Components
+
+export { default as AppBar } from './components/AppBar';
+export { default as BookDetail } from './components/BookDetail';
+export { default as ConnectivityWarning } from './components/ConnectivityWarning';
+export { default as ContactDetail } from './components/ContactDetail';
+export { default as CourseDetail } from './components/CourseDetail';
+export { default as CourseDetailDialog } from './components/CourseDetailDialog';
+export { default as CourseInfo } from './components/CourseInfo';
+export { default as DevelopmentDialog } from './components/DevelopmentDialog';
+export { default as EventCodeInput } from './components/EventCodeInput';
+export { default as FlexMenuEntry } from './components/FlexMenuEntry';
+export { default as MainMenuEntry } from './components/MainMenuEntry';
+export { default as MealItem } from './components/MealItem';
+export { default as MensaMenu } from './components/MensaMenu';
+export { default as MensaSlider } from './components/MensaSlider';
+export { default as Modal } from './components/Modal';
+export { default as NewsDetail } from './components/NewsDetail';
+export { default as NewsList } from './components/NewsList';
+export { default as NewsListItem } from './components/NewsListItem';
+export { default as OtherCourses } from './components/OtherCourses';
+export { default as PtsDeparture } from './components/PtsDeparture';
+export { default as PtsStation } from './components/PtsStation';
+export { default as QuickLinks } from './components/QuickLinks';
+export { default as RoomDetail } from './components/RoomDetail';
+export { default as ScaledImage } from './components/ScaledImage';
+export { default as SearchResults } from './components/SearchResults';
+export { default as SettingSection } from './components/SettingSection';
+export { default as SettingsDialog } from './components/SettingsDialog';
+export { default as SettingsDialogRadio } from './components/SettingsDialogRadio';
+export { default as SettingsDialogSelect } from './components/SettingsDialogSelect';
+export { default as TimetableCodeInput } from './components/TimetableCodeInput';
+export { default as TimetableDay } from './components/TimetableDay';
+export { default as TimetableEvent } from './components/TimetableEvent';
+export { default as TimetableList } from './components/TimetableList';
+export { default as TimetableMonth } from './components/TimetableMonth';
+export { default as TimetableWeek } from './components/TimetableWeek';
+export { default as TopNews } from './components/TopNews';
+export { default as TopNewsHtwk } from './components/TopNewsHtwk';
+export { default as WebView } from './components/WebView';
+export { default as ComponentJobsFilter } from './components/ComponentJobsFilter';
+
+
